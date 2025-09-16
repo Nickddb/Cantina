7a72ec63a719c1ff4afc64efad002895bbeaf7ad
Nickddb <nicollerocha.dev2024>
2025-09-16 15:30:55 -0300

    ta tudo certinho agora

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
 document.addEventListener('DOMContentLoaded', () =>{
     fetchProdutos();
     //Seleciona todos os botões da classe "add-To-Cart-btn"
-    const addToCartButtons = document.querySelectorAll('.adicionar-littlecar');
-
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', () =>{
-            //quando o botão for clicado, o eventlistener vai ser executado
-            const card = button.closest('.card');
-            const productName = card.getAttribute('data-nome');
-            const productPrice = parseFloat(card.getAttribute('data-price'));
-
-            const product = { //cria um objeto 'product' p/ guardar info do item
-                name: productName,
-                price: productPrice,
-            };
-
-            let cart = JSON.parse(localStorage.getItem('cart')) || [];
-            cart.push(product)
-
-            localStorage.setItem('cart', JSON.stringify(cart));
-
-            alert(`${productName} foi adicionado ao carrinho!`)
-        })
-    })
     const cardItemsContainer = document.getElementById('card-items-container');
     const cardToTalValue = document.getElementById('card-total-value');
     const checkoutBtn = document.getElementById('checkout-btn');
@@ -95,5 +73,32 @@ function renderProdutos(produtos){
                         <button class="adicionar-littlecar">COMPRAR</button>`;
             container.appendChild(card);
         }
+    });
+    addLittleCar();
+}
+
+function addLittleCar(){
+    const addToCartButtons = document.querySelectorAll('.adicionar-littlecar');
+
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () =>{
+            //quando o botão for clicado, o eventlistener vai ser executado
+            const card = button.closest('.card');
+            const productName = card.getAttribute('data-name');
+            const productPrice = parseFloat(card.getAttribute('data-price'));
+
+            const product = { //cria um objeto 'product' p/ guardar info do item
+                name: productName,
+                price: productPrice,
+            };
+
+            let cart = JSON.parse(localStorage.getItem('cart')) || [];
+            cart.push(product)
+
+            localStorage.setItem('cart', JSON.stringify(cart));
+
+            alert(`${productName} foi adicionado ao carrinho!`)
+        })
     })
+
 }
